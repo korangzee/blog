6307d4c89070a3c2a04a86776b033449686e87b0
hyungjinko <hyungjinko@snu.ac.kr>
2020-01-07 19:40:18 +0900

    init

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -9,7 +9,7 @@
            내 인스타그램 내의 사진을 받아와 보자
         </v-card-text>
         <v-card-action>
-          <v-btn>
+          <v-btn href="https://api.instagram.com/oauth/authorize?app_id=2461918490735728&redirect_uri=https://korangzee.github.io/blog/auth/&scope=user_profile,user_media&response_type=code">
             받아오기
           </v-btn>
         </v-card-action>
@@ -23,9 +23,9 @@
           </v-card-title>
           <v-divider></v-divider>
           <v-card-text>
-              <v-col v-for="item in items" cols="4">
+              <!-- <v-col v-for="item in items" cols="4">
                   <v-img aspect-ratio="1" :src="item.data().url"></v-img>
-              </v-col>
+              </v-col> -->
           </v-card-text>
       </v-card>
     </v-col>
@@ -39,7 +39,31 @@ export default {
     return {
       items: [],
     }
+  },
+  methods: {
+    async getFromInsta(){
+
+            // GET 요청으로 code를 받아온다.
+                // let res = await axios({
+                //     method: 'get',
+                //     url: 'https://api.instagram.com/oauth/authorize?app_id=2461918490735728&redirect_uri=https://korangzee.github.io/blog/&scope=user_profile,user_media&response_type=code',
+                // })
+
+                // console.log(res)
+
+
+
+
+            // code를 활용하여 액세스 토큰을 받아온다
+            // 액세스 토큰을 활용하여 고객의 미디어 아이디를 받아온다
+            // 각 미디어 아이디로부터 정보를 받아와서 firestore에 기록하고, storage에 다운로드한다.
+
+            // let refs = await this.$storage.child('userProfiles').child(this.currentUser.uid).child('photoLibrary')
+            // console.log(refs)
+            // console.log(refs.child.length)
+            // let url = await refs.getDownloadURL()
+            // let name = refs.name
+      }
   }
-  
 }
 </script>
