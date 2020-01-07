8f5101e2b9c3c73d8ab3f6985ecc7359623962a6
hyungjinko <hyungjinko@snu.ac.kr>
2020-01-07 19:15:51 +0900

    auth.vue

---
+++ pages/auth/auth.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col align="center">
+      <v-card flat>
+        <v-card-title>
+          소개
+        </v-card-title>
+        <v-card-text>
+            <p>소속: 서울대학교 산업공학과</p>
+            <p>과정: 박사 과정</p>
+            <p>연구 분야: Machine Learning | Deep Learning | Application for Finance</p>
+            <p>취미: web,app programming | algorithmic trading</p>
+            <p>기술 스택: Python, R, tensorflow, keras, Vue.js, Nuxt.js, javascript, django, sklearn, firebase, serverless architecture, NoSQL</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
